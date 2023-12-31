0418e9e9da00dd17df5a06764c24d2268c6fb538
NR Habibi <nrhbb14@gmail.com>
2023-12-31 09:50:10 +0800

    rename typo code

--- server/api/movies/[id].js
+++ server/api/movies/[id].js
@@ -1,10 +1,11 @@
 export default defineEventHandler(async (event) => {
-    const {moviekey} = useRuntimeConfig()
+    const {movieKey} = useRuntimeConfig()
     const movieid = event.context.params.id
     try{
-        const data = await $fetch(`https://api.themoviedb.org/3/movie/${movieid}?api_key=${moviekey}&language=en-US&page=1`)
+        const data = await $fetch(`https://api.themoviedb.org/3/movie/${movieid}?api_key=${movieKey}&language=en-US`)
         return data 
     }    catch (error){
+        console.error(error);
         return null
     }
 })
--- server/api/movies/index.js
+++ server/api/movies/index.js
@@ -1,7 +1,7 @@
 export default defineEventHandler(async (event) => {
-    const {moviekey} = useRuntimeConfig()
+    const {movieKey} = useRuntimeConfig()
     try{
-        const data = await $fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${moviekey}`)
+        const data = await $fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${movieKey}`)
         return data 
     }    catch (error){
         return null
